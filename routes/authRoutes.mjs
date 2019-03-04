1b884f2f2f39483cecef41861769c068a746ea85
waraphon roonnapai <eepw0211@gmail.com>
2019-03-04 17:27:45 +0700

    fix code

--- routes/authRoutes.mjs
+++ routes/authRoutes.mjs
@@ -11,9 +11,19 @@ router.get('/auth/google', passport.authenticate('google', {
   scope: ['profile', 'email']
 }))
 
-router.get('/auth/google/callback', passport.authenticate('google'), (req, res) => {
-  res.send('callback page')
-})
+router.get('/auth/google/callback',
+  passport.authenticate('google'), // complete the authenticate using the google strategy
+  (err, req, res, next) => { // custom error handler to catch any errors, such as TokenError
+    if (err.name === 'TokenError') {
+     res.redirect('/auth/google'); // redirect them back to the login page
+    } else {
+     // Handle other errors here
+    }
+  },
+  (req, res) => { // On success, redirect back to '/'
+    res.redirect('/');
+  }
+);
 
 router.get('/api/logout', (req, res) => {
   req.logOut()
